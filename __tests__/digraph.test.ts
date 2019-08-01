c86ddea0cdbb98b6d74c8ef3aebfb8be55f5be57
concefly <concefly@foxmail.com>
2019-08-01 11:38:15 +0800

    feat: digraph findAllTrace

---
+++ __tests__/digraph.test.ts
@@ -0,0 +1,64 @@
+import { findAllTrace, IDigraphNode } from '../src/digraph';
+
+describe('findAllTrace', () => {
+  const simplify = (result: IDigraphNode[][]) => result.map(r => r.map(_r => _r.id).join(','));
+
+  it('normal', () => {
+    const g: IDigraphNode[] = [
+      {
+        id: '0',
+        nextIds: ['1', '2'],
+      },
+      {
+        id: '1',
+        nextIds: ['2', '3', '4'],
+      },
+      {
+        id: '2',
+        nextIds: ['4'],
+      },
+      {
+        id: '3',
+        nextIds: ['4', '5'],
+      },
+      {
+        id: '4',
+        nextIds: ['5'],
+      },
+      {
+        id: '5',
+        nextIds: [],
+      },
+    ];
+    const result = findAllTrace(g, '0', '5');
+    expect(simplify(result)).toEqual(['0,1,2,4,5', '0,1,3,4,5', '0,1,3,5', '0,1,4,5', '0,2,4,5']);
+  });
+
+  it('有环', () => {
+    const result = findAllTrace(
+      [
+        { id: '0', nextIds: ['1'] },
+        { id: '1', nextIds: ['2'] },
+        { id: '2', nextIds: ['1', '3'] },
+        { id: '3', nextIds: ['1', '4'] },
+        { id: '4', nextIds: [] },
+      ],
+      '0',
+      '4'
+    );
+
+    expect(simplify(result)).toEqual(['0,1,2,3,4']);
+  });
+
+  it.skip('爆栈', () => {
+    const g: IDigraphNode[] = [];
+    for (let i = 0; i <= 9999; i++) {
+      g.push({
+        id: i + '',
+        nextIds: [i + 1 + ''],
+      });
+    }
+
+    findAllTrace(g, '0', '9999');
+  });
+});
---
+++ src/digraph.ts
@@ -0,0 +1,32 @@
+export interface IDigraphNode {
+  id: string;
+  nextIds: string[];
+}
+
+/** 两点之间所有路径 */
+export const findAllTrace = <G extends IDigraphNode>(graph: G[], startId: string, stopId: string) => {
+  const idMap = new Map<string, G>();
+  graph.forEach(g => idMap.set(g.id, g));
+
+  const traceSet = new Set<G[]>();
+
+  const _walk = (_id: string, _trace: G[]) => {
+    const current = idMap.get(_id);
+    if (!current) return;
+
+    if (_trace.find(t => t.id === current.id)) return;
+
+    if (current.id === stopId) {
+      traceSet.add([..._trace, current]);
+      return;
+    }
+
+    current.nextIds.forEach(nextId => {
+      _walk(nextId, [..._trace, current]);
+    });
+  };
+
+  _walk(startId, []);
+
+  return [...traceSet.values()];
+};
