05f4975438d6cded0209d1d8bf0326a34461684d
huize.hwj <huize.hwj@alipay.com>
2020-09-30 13:17:32 +0800

    feat: Digraph

--- __tests__/digraph.test.ts
+++ __tests__/digraph.test.ts
@@ -1,58 +1,48 @@
-import { IDigraphNode, getAllTraceGenerator } from '../src/digraph';
+import { IDigraphNode, Digraph } from '../src/digraph';
 
 const simplify = (result: IDigraphNode[][]) => result.map(r => r.map(_r => _r.id).join(','));
 
-describe('getAllTraceGenerator', () => {
-  it('normal', () => {
-    const g: IDigraphNode[] = [
-      {
-        id: '0',
-        nextIds: ['1', '2'],
-      },
-      {
-        id: '1',
-        nextIds: ['2', '3', '4'],
-      },
-      {
-        id: '2',
-        nextIds: ['4'],
-      },
-      {
-        id: '3',
-        nextIds: ['4', '5'],
-      },
-      {
-        id: '4',
-        nextIds: ['5'],
-      },
-      {
-        id: '5',
-        nextIds: [],
-      },
-    ];
-    const result = getAllTraceGenerator(g, '0', '5');
-    expect(simplify([...result])).toEqual([
-      '0,1,2,4,5',
-      '0,1,3,4,5',
-      '0,1,3,5',
-      '0,1,4,5',
-      '0,2,4,5',
-    ]);
+const normalGraph: IDigraphNode[] = [
+  { id: '0', nextIds: ['1', '2'] },
+  { id: '1', nextIds: ['2', '3', '4'] },
+  { id: '2', nextIds: ['4'] },
+  { id: '3', nextIds: ['4', '5', '7'] },
+  { id: '4', nextIds: ['5', '6'] },
+  { id: '5', nextIds: [] },
+  { id: '6', nextIds: [] },
+  { id: '7', nextIds: [] },
+];
+
+const circleGraph: IDigraphNode[] = [
+  { id: '0', nextIds: ['1'] },
+  { id: '1', nextIds: ['2'] },
+  { id: '2', nextIds: ['1', '3'] },
+  { id: '3', nextIds: ['1', '4'] },
+  { id: '4', nextIds: [] },
+];
+
+describe('Digraph', () => {
+  describe('getAllTraceList', () => {
+    it('normal', () => {
+      const result = new Digraph(normalGraph).getAllTraceList('0', '5');
+      expect(simplify(result)).toEqual(['0,1,2,4,5', '0,1,3,4,5', '0,1,3,5', '0,1,4,5', '0,2,4,5']);
+    });
+
+    it('有环', () => {
+      const result = new Digraph(circleGraph).getAllTraceList('0', '4');
+      expect(simplify(result)).toEqual(['0,1,2,3,4']);
+    });
   });
 
-  it('有环', () => {
-    const result = getAllTraceGenerator(
-      [
-        { id: '0', nextIds: ['1'] },
-        { id: '1', nextIds: ['2'] },
-        { id: '2', nextIds: ['1', '3'] },
-        { id: '3', nextIds: ['1', '4'] },
-        { id: '4', nextIds: [] },
-      ],
-      '0',
-      '4'
-    );
+  describe('getOneTrace', () => {
+    it('normal', () => {
+      const result = new Digraph(normalGraph).getOneTrace('0', '7')!;
+      expect(simplify([result])).toEqual(['0,1,3,7']);
+    });
 
-    expect(simplify([...result])).toEqual(['0,1,2,3,4']);
+    it('有环', () => {
+      const result = new Digraph(circleGraph).getOneTrace('0', '4')!;
+      expect(simplify([result])).toEqual(['0,1,2,3,4']);
+    });
   });
 });
--- src/digraph.ts
+++ src/digraph.ts
@@ -3,6 +3,7 @@ export interface IDigraphNode {
   nextIds: string[];
 }
 
+/** @deprecated */
 export const getAllTraceGenerator = <G extends IDigraphNode>(
   graph: G[],
   startId: string,
@@ -32,3 +33,126 @@ export const getAllTraceGenerator = <G extends IDigraphNode>(
 
   return _getRestTraceList(startId);
 };
+
+export class Digraph<T extends IDigraphNode> {
+  constructor(readonly graph: T[]) {}
+
+  public readonly idMap = new Map<string, T>(this.graph.map(g => [g.id, g]));
+
+  /** 深度优先遍历 */
+  public walk(
+    startId: string,
+    /** 前序遍历 */
+    tapVLR: (
+      t: T,
+      opt: {
+        /** walk 踪迹 */
+        trace: T[];
+        isLeaf: boolean;
+        nextList: T[];
+        stopWalkIn: () => void;
+        exit: () => void;
+      }
+    ) => void,
+    /** 后序遍历 */
+    tapLRD?: (
+      t: T,
+      opt: {
+        /** walk 踪迹 */
+        trace: T[];
+        isLeaf: boolean;
+        nextList: T[];
+      }
+    ) => void
+  ) {
+    const _walk = (cid: string, lastTrace: T[]) => {
+      // 防成环死循环
+      if (lastTrace.some(t => t.id === cid)) return;
+
+      const current = this.idMap.get(cid);
+      if (!current) return;
+
+      const nextIds = current.nextIds;
+      const nextList: T[] = nextIds.map(nid => this.idMap.get(nid)).filter(v => !!v) as any;
+      const isLeaf = nextList.length === 0;
+
+      const ns = {
+        stopWalkIn: false,
+        exit: false,
+      };
+      const stopWalkIn = () => (ns.stopWalkIn = true);
+      const exit = () => (ns.exit = true);
+
+      const trace = lastTrace.concat(current);
+
+      // tap 自己
+      tapVLR(current, { stopWalkIn, exit, trace, isLeaf, nextList });
+
+      if (ns.exit) throw new Error('__walk_exit__');
+
+      // 尝试递归 tap 儿子
+      if (!ns.stopWalkIn && !isLeaf) nextList.forEach(next => _walk(next.id, trace));
+
+      // 后续遍历
+      tapLRD?.(current, { trace, isLeaf, nextList });
+    };
+
+    try {
+      _walk(startId, []);
+    } catch (e) {
+      if (e.message === '__walk_exit__') {
+        // 调用方主动退出，什么都不干
+      } else {
+        // 否则继续往上抛
+        throw e;
+      }
+    }
+  }
+
+  /** 返回所有踪迹 */
+  public getAllTraceList(startId: string, stopId: string): T[][] {
+    const re: T[][] = [];
+
+    this.walk(
+      startId,
+      // 前序
+      (cur, { stopWalkIn: done, trace }) => {
+        // 遇到停止点 -> 记录并停止
+        if (cur.id === stopId) {
+          re.push(trace);
+          return done();
+        }
+      }
+    );
+
+    return re;
+  }
+
+  /** 返回最先找到的一条踪迹 */
+  public getOneTrace(startId: string, stopId: string): T[] | undefined {
+    let re: T[];
+    const blockIds = new Set<string>();
+
+    this.walk(
+      startId,
+      // 前序
+      (cur, { stopWalkIn, exit, trace }) => {
+        // block 点 -> 停止当前链路搜索
+        if (blockIds.has(cur.id)) return stopWalkIn();
+
+        // 遇到停止点 -> 记录并停止
+        if (cur.id === stopId) {
+          re = trace;
+          return exit();
+        }
+      },
+      // 后序
+      cur => {
+        // 如果深度优先遍历退出的时候还没有找到 trace，则 block 此节点
+        if (!re) blockIds.add(cur.id);
+      }
+    );
+
+    return re!;
+  }
+}
